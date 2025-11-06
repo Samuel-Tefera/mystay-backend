6daf43c953a1afdfe1c829727885fc89ac8f728f
Samuel-Tefera <samuelteferab@gmail.com>
2025-11-06 11:34:50 +0300

    add relationship to hotel on hotel manager model

--- app/models/users.py
+++ app/models/users.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -13,3 +14,5 @@ class HotelManager(Base):
   email = Column(String, nullable=False, unique=True)
   phone = Column(String, nullable=True)
   password = Column(String, nullable=True)
+
+  hotel = relationship('Hotel', back_populates='manager', uselist=False)
