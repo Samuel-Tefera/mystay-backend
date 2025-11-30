4c73f13af096db15251ecfa9436f6267bed1b319
Samuel-Tefera <samuelteferab@gmail.com>
2025-11-30 15:50:30 +0300

    add crud function on hotel application

--- app/crud/hotel.py
+++ app/crud/hotel.py
@@ -1,8 +1,36 @@
 from sqlalchemy.orm import Session
-from app.models.hotel import Hotel
+from app.models.hotel import Hotel, HotelApplication
 
-from app.schemas.hotel import HotelCreate
+from app.schemas.hotel import HotelApplicationCreate, HotelCreate
 
+
+# Creating new hotel application
+def create_new_hotel_application(db: Session, new_hotel_application: HotelApplicationCreate):
+  db_hotel_application = HotelApplication(
+    manager_name = new_hotel_application.manager_name,
+    manager_email = new_hotel_application.manager_email,
+    manager_phone = new_hotel_application.manager_phone,
+
+    hotel_name = new_hotel_application.hotel_name,
+    hotel_address = new_hotel_application.hotel_address,
+    hotel_description = new_hotel_application.hotel_description,
+    hotel_star_rating = new_hotel_application.hotel_star_rating,
+    hotel_exact_location = new_hotel_application.hotel_exact_location.model_dump(),
+  )
+
+  db.add(db_hotel_application)
+  db.commit()
+  db.refresh(db_hotel_application)
+
+  return db_hotel_application
+
+# Get all hotel applications
+def get_all_hotel_applications(db : Session):
+  return db.query(HotelApplication).all()
+
+# Get hotel application detail
+def get_hotel_application_detail(db: Session, application_id: int):
+  return db.query(HotelApplication).filter(HotelApplication.id == application_id).first()
 
 # Creating new Hotel
 def create_new_hotel(db: Session, new_hotel: HotelCreate):
@@ -13,8 +41,6 @@ def create_new_hotel(db: Session, new_hotel: HotelCreate):
     address = new_hotel.address,
     rating = new_hotel.rating,
     exact_location = new_hotel.exact_location.dict(),
-    contact_number = new_hotel.contact_number,
-    email = new_hotel.email
   )
 
   db.add(db_hotel)
