a0ad7722a59ee30916c61a34768c18378378e5b5
Samuel-Tefera <samuelteferab@gmail.com>
2025-12-01 16:01:44 +0300

    create admin model

--- app/models/users.py
+++ app/models/users.py
@@ -1,22 +1,39 @@
 from datetime import datetime
-from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy import Column, DateTime, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 
 from app.database import Base
 
 
-# For a demo purpose need modification later
-class HotelManager(Base):
-  '''Hotel Manager Model'''
-  __tablename__ = 'hotel_managers'
+class Admin(Base):
+  """Admin user model"""
+  __tablename__ = 'admins'
 
   id = Column(Integer, primary_key=True, index=True)
-  name = Column(String, nullable=False)
-  email = Column(String, nullable=False, unique=True)
-  phone = Column(String, nullable=True)
-  password = Column(String, nullable=True)
+  email = Column(String, unique=True, nullable=False)
+  password_hash = Column(String, nullable=False)
+  full_name = Column(String, nullable=False)
+  created_at = Column(DateTime, default=datetime.utcnow)
 
-  hotel = relationship('Hotel', back_populates='manager', uselist=False)
+
+class HotelManager(Base):
+    """Hotel Manager user Model"""
+    __tablename__ = "hotel_managers"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True, index=True)
+    phone = Column(String, nullable=True)
+
+    is_approved = Column(Boolean, default=False) # Magic link authentication
+
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    last_login = Column(DateTime, nullable=True)
+
+    # Relation to hotel
+    hotel = relationship("Hotel", back_populates="manager")
 
 
 # Guest User
