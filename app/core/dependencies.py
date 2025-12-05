49688186503d0e86e5b15340ceb08b4e19413332
Samuel-Tefera <samuelteferab@gmail.com>
2025-12-05 22:14:29 +0300

    function to get current login hotel manager

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,10 +1,18 @@
 from fastapi import Depends, HTTPException
-from app.core.security import decode_token, oauth2_scheme
+from app.core.security import decode_token, admin_oauth2_scheme, manager_oauth2_scheme
 
-def require_admin(token: str = Depends(oauth2_scheme)):
+def require_admin(token: str = Depends(admin_oauth2_scheme)):
     payload = decode_token(token)
 
-    if payload.get("role") != "admin":
-        raise HTTPException(status_code=403, detail="Admins only")
+    if payload.get('role') != 'admin':
+        raise HTTPException(status_code=403, detail='Admins only')
+
+    return payload
+
+def require_hotel_manager(token: str = Depends(manager_oauth2_scheme)):
+    payload = decode_token(token)
+
+    if payload.get('role') != 'manager':
+        raise HTTPException(status_code=403, detail="Hotel Mangers only")
 
     return payload
