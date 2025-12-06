2da6b02840dddcae1a46de09bcd1a251c66c3a3b
Samuel-Tefera <samuelteferab@gmail.com>
2025-12-06 11:31:13 +0300

    add route to update hotel

--- app/routes/hotel.py
+++ app/routes/hotel.py
@@ -2,17 +2,14 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from sqlalchemy.orm import Session
 
+from app.core.dependencies import require_hotel_manager
 from app.database import get_db
-from app.schemas.hotel import HotelCreate, HotelDisplay
-from app.crud.hotel import create_new_hotel, get_all_hotels, get_one_hotel
+from app.models.hotel import Hotel
+from app.schemas.hotel import HotelDisplay, HotelDisplayDetail, HotelUpdate
+from app.crud.hotel import get_all_hotels, get_one_hotel
 
 
 router = APIRouter(prefix='/hotels', tags=['Hotels'])
-
-# Create Hotel
-@router.post('/', response_model=HotelDisplay)
-def create_hotel(new_hotel: HotelCreate, db: Session = Depends(get_db)):
-  return create_new_hotel(db, new_hotel)
 
 # Get all Hotels
 @router.get('/', response_model=list[HotelDisplay])
@@ -20,11 +17,36 @@ def get_hotels(db: Session = Depends(get_db)):
   return get_all_hotels(db)
 
 # Get one Hotel by ID
-@router.get('/{hotel_id}', response_model=HotelDisplay)
-def get_hotel(hotel_id, db: Session = Depends(get_db)):
+@router.get('/{hotel_id}', response_model=HotelDisplayDetail)
+def get_hotel(hotel_id: int, db: Session = Depends(get_db)):
   db_hotel = get_one_hotel(db, hotel_id)
 
   if not db_hotel:
     raise HTTPException(status_code=404, detail='Hotel not found')
 
   return db_hotel
+
+# Update hotel
+@router.patch('/{hotel_id}', response_model=HotelDisplay)
+def update_hotel(
+  hotel_id: int,
+  hotel_update: HotelUpdate,
+  db: Session = Depends(get_db),
+  token_data = Depends(require_hotel_manager)):
+
+  hotel = db.query(Hotel).filter(Hotel.id == hotel_id).first()
+
+  if not hotel:
+    raise HTTPException(status_code=404, detail='Hotel not found.')
+
+  if hotel.manager_id != int(token_data['sub']):
+    raise HTTPException(status_code=403, detail='Not allowed')
+
+  update_data = hotel_update.model_dump(exclude_unset=True)
+
+  for key, value in update_data.items():
+      setattr(hotel, key, value)
+
+  db.commit()
+  db.refresh(hotel)
+  return hotel
