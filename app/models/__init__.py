8b7c2d41153f3dd8a419ed0804afa52ecf43aaf9
Samuel-Tefera <samuelteferab@gmail.com>
2025-12-08 11:06:58 +0300

    include booking and payment model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,5 @@
 from .users import Admin, HotelManager, Guest
 from .hotel import Hotel, HotelApplication
 from .room import Room
+from .bookings import Booking
+from .payments import Payment
--- app/utils/create_tables.py
+++ app/utils/create_tables.py
@@ -2,6 +2,8 @@ from app.database import Base, engine
 from app.models.users import HotelManager, Guest
 from app.models.hotel import Hotel
 from app.models.room import Room
+from app.models.bookings import Booking
+from app.models.payments import Payment
 
 print("Creating tables in database...")
 Base.metadata.create_all(bind=engine)
