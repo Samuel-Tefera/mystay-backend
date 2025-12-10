41ef430b32d4cef9dca80a620504baa42e6eca0d
Samuel-Tefera <samuelteferab@gmail.com>
2025-12-10 21:27:55 +0300

    create booking schemas

---
+++ app/schemas/bookings.py
@@ -0,0 +1,24 @@
+from datetime import date
+from pydantic import BaseModel
+
+from app.models.bookings import BookingStatus
+from app.models.payments import PaymentMethod
+
+
+class BookingBase(BaseModel):
+  check_in: date
+  check_out: date
+
+
+class BookingCreate(BookingBase):
+  room_id: int
+  payment_method: PaymentMethod
+
+
+class BookingDisplay(BookingBase):
+  id: int
+  status: BookingStatus
+  total_price: float
+
+  class Config:
+    orm_mode: True
