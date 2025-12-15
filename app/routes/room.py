916101ac5f55a53cc030727efce718edd2ab5940
Samuel-Tefera <samuelteferab@gmail.com>
2025-12-15 11:37:39 +0300

    update post route to create new room

--- app/routes/room.py
+++ app/routes/room.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, File, Form, HTTPException, UploadFile
 
 from sqlalchemy.orm import Session
+
 from app.core.dependencies import require_hotel_manager
 from app.crud.room import create_new_room, get_all_rooms, get_one_room
 from app.models.hotel import Hotel
-from app.models.room import Room
-
+from app.models.room import BedType, Room, RoomType
 from app.schemas.room import RoomBase, RoomDisaply, RoomUpdate
 from app.database import get_db
+from app.utils.room_service import save_room_image
 
 
 router = APIRouter(prefix='/hotels', tags=['Rooms'])
@@ -29,8 +30,28 @@ def get_room(hotel_id, room_id, db: Session = Depends(get_db)):
 
 # Create new room for Hotel
 @router.post('/{hotel_id}/rooms', response_model=RoomDisaply, dependencies=[Depends(require_hotel_manager)])
-def create_room(hotel_id, new_room: RoomBase, db: Session = Depends(get_db)):
-  return create_new_room(hotel_id, new_room, db)
+def create_room(
+  hotel_id: int,
+  room_number: str = Form(...),
+  room_type: RoomType = Form(...),
+  price_per_night: float = Form(...),
+  description: str = Form(...),
+  bed_type: BedType = Form(...),
+  image: UploadFile = File(...),
+  db: Session = Depends(get_db)
+  ):
+
+  new_room = RoomBase(
+    room_number=room_number,
+    room_type=room_type,
+    price_per_night=price_per_night,
+    description=description,
+    bed_type=bed_type,
+    )
+
+  image_url = save_room_image(image)
+
+  return create_new_room(hotel_id, new_room, image_url, db)
 
 # Update room
 @router.patch('/{hotel_id}/rooms/{room_id}')
