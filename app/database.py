bb07a5cf4816a3bbaf632a4eec3acfb7d1c14c0c
Samuel-Tefera <samuelteferab@gmail.com>
2026-01-02 16:47:25 +0300

    feat: change database from sqllite to supabase postgres

--- app/database.py
+++ app/database.py
@@ -1,20 +1,30 @@
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=10
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
 
-# Dependency function to get database session
 def get_db():
     db = SessionLocal()
     try:
